1f941dbfdda73d81fd0bdd79c11235352e201f6d
Ishan Dey <ishandey0534@gmail.com>
2020-06-17 14:03:56 +0530

    Optimized webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+module.exports = (env) => {
+  const isProduction = (env==='production');
+  const MiniCSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.s?css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
         }
-      },
-      {
-        test: /\.s?css$/i,
-        use: ['style-loader', 'css-loader','sass-loader'],
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public')
-  }
-};
+      ]
+    },
+    plugins: [MiniCSSExtract],
+    devtool: isProduction?'source-map':'inline-source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public')
+    }
+  };
+}
